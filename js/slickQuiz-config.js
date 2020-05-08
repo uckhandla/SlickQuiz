c761a400cfa017764c9971fa861b05e9f8a1981e
Ujamshi Khandla <uckhandla.dev@gmail.com>
2020-05-08 07:27:25 +0530

    Update slickQuiz-config.js

--- js/slickQuiz-config.js
+++ js/slickQuiz-config.js
@@ -1,8 +1,4 @@
-// Setup your quiz text and questions here
 
-// NOTE: pay attention to commas, IE struggles with those bad boys
-
-var quizJSON = {
     "info": {
         "name":    "Test Your Knowledge!!",
         "main":    "<p>Think you're smart enough to be on Jeopardy? Find out with this super crazy knowledge quiz!</p>",
@@ -11,62 +7,62 @@ var quizJSON = {
         "level2":  "Jeopardy Contender",
         "level3":  "Jeopardy Amateur",
         "level4":  "Jeopardy Newb",
-        "level5":  "Stay in school, kid..." // no comma here
+        "level5":  "Stay in school, kid..." 
     },
     "questions": [
-        { // Question 1 - Multiple Choice, Single True Answer
+        { 
             "q": "What number is the letter A in the English alphabet?",
             "a": [
                 {"option": "8",      "correct": false},
                 {"option": "14",     "correct": false},
                 {"option": "1",      "correct": true},
-                {"option": "23",     "correct": false} // no comma here
+                {"option": "23",     "correct": false} 
             ],
             "correct": "<p><span>That's right!</span> The letter A is the first letter in the alphabet!</p>",
-            "incorrect": "<p><span>Uhh no.</span> It's the first letter of the alphabet. Did you actually <em>go</em> to kindergarden?</p>" // no comma here
+            "incorrect": "<p><span>Uhh no.</span> It's the first letter of the alphabet. Did you actually <em>go</em> to kindergarden?</p>" 
         },
-        { // Question 2 - Multiple Choice, Multiple True Answers, Select Any
+        { 
             "q": "Which of the following best represents your preferred breakfast?",
             "a": [
                 {"option": "Bacon and eggs",               "correct": false},
                 {"option": "Fruit, oatmeal, and yogurt",   "correct": true},
                 {"option": "Leftover pizza",               "correct": false},
-                {"option": "Eggs, fruit, toast, and milk", "correct": true} // no comma here
+                {"option": "Eggs, fruit, toast, and milk", "correct": true} 
             ],
             "select_any": true,
             "correct": "<p><span>Nice!</span> Your cholestoral level is probably doing alright.</p>",
-            "incorrect": "<p><span>Hmmm.</span> You might want to reconsider your options.</p>" // no comma here
+            "incorrect": "<p><span>Hmmm.</span> You might want to reconsider your options.</p>" 
         },
-        { // Question 3 - Multiple Choice, Multiple True Answers, Select All
+        { 
             "q": "Where are you right now? Select ALL that apply.",
             "a": [
                 {"option": "Planet Earth",           "correct": true},
                 {"option": "Pluto",                  "correct": false},
                 {"option": "At a computing device",  "correct": true},
-                {"option": "The Milky Way",          "correct": true} // no comma here
+                {"option": "The Milky Way",          "correct": true} 
             ],
             "correct": "<p><span>Brilliant!</span> You're seriously a genius, (wo)man.</p>",
-            "incorrect": "<p><span>Not Quite.</span> You're actually on Planet Earth, in The Milky Way, At a computer. But nice try.</p>" // no comma here
+            "incorrect": "<p><span>Not Quite.</span> You're actually on Planet Earth, in The Milky Way, At a computer. But nice try.</p>" 
         },
-        { // Question 4
+        { 
             "q": "How many inches of rain does Michigan get on average per year?",
             "a": [
                 {"option": "149",    "correct": false},
                 {"option": "32",     "correct": true},
                 {"option": "3",      "correct": false},
-                {"option": "1291",   "correct": false} // no comma here
+                {"option": "1291",   "correct": false} 
             ],
             "correct": "<p><span>Holy bananas!</span> I didn't actually expect you to know that! Correct!</p>",
-            "incorrect": "<p><span>Fail.</span> Sorry. You lose. It actually rains approximately 32 inches a year in Michigan.</p>" // no comma here
+            "incorrect": "<p><span>Fail.</span> Sorry. You lose. It actually rains approximately 32 inches a year in Michigan.</p>" 
         },
-        { // Question 5
+        { 
             "q": "Is Earth bigger than a basketball?",
             "a": [
                 {"option": "Yes",    "correct": true},
-                {"option": "No",     "correct": false} // no comma here
+                {"option": "No",     "correct": false} 
             ],
             "correct": "<p><span>Good Job!</span> You must be very observant!</p>",
-            "incorrect": "<p><span>ERRRR!</span> What planet Earth are <em>you</em> living on?!?</p>" // no comma here
-        } // no comma here
+            "incorrect": "<p><span>ERRRR!</span> What planet Earth are <em>you</em> living on?!?</p>" 
+        } 
     ]
-};
+
